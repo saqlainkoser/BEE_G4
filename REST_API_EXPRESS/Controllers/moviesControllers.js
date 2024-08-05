ddfcde85ffcdce4477357308b7bd77e43763e264
saqlainkoser <saqlainkoser@gmail.com>
2024-08-05 11:56:52 +0530

    changed

--- REST_API_EXPRESS/Controllers/moviesControllers.js
+++ REST_API_EXPRESS/Controllers/moviesControllers.js
@@ -40,18 +40,46 @@ exports.getAllMovies =async (req,res)=>{
         querystr=querystr.replace(/\b(gte|gt|lte|lt)\b/g,(match)=>`$${match}`)
 
         const queryObj= JSON.parse(querystr)
-        console.log(queryObj)
-        const movies =await Movie.find(req.query);
+        // console.log(queryObj)
+        // const movies =await Movie.find(req.query);
 
         // {duration:{$gte : 118}, gte}
         //conditions -  gte / lte / gt / lt
         //find({name="shaan",age:{$gte:18})
 
+        //sorting data 
+        let query = Movie.find()
+        // console.log(req.query.sort);
 
+        if(req.query.sort){
+            query = query.sort(req.query.sort)
+        }else{
+            // query = query.sort("-createdAt")
+        }
 
+        //LIMITING FIELDS
+        if(req.query.fields){
+            let fields = req.query.fields.split(',').join(" ")
+            query = query.select(fields)
+        }
+        
+        //Pagination and Limits 
+        let page= req.query.page * 1  || 1;
+        let limits=req.query.limit * 1 || 10;
+        
+        // 123456789101112
+        //p1 -s0 , p2 - s3 , p3 - s6
+        //l3
+        const skip = (page - 1) * limits
+        console.log(skip)
+        
+        query = query.skip(skip).limit(limits)
+
+        const movies =await query;
 
         res.status(200).json({
             status:"success",
+            length:movies.length,
             data:{
                 movies
             }
@@ -63,6 +91,7 @@ exports.getAllMovies =async (req,res)=>{
         })
     } 
 }
+
 
 exports.createMovie = async(req,res)=>{
     try{
